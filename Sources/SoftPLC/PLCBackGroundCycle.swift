b2d724662c4dd56505170dc62e06dd3096764d96
TheMisfit68 <janverrept@me.com>
2021-01-31 11:27:39 +0100

    Cycletime monitored now

--- Sources/SoftPLC/PLCBackGroundCycle.swift
+++ Sources/SoftPLC/PLCBackGroundCycle.swift
@@ -9,45 +9,65 @@ import Foundation
 import JVCocoa
 
 class PLCBackgroundCycle:ObservableObject {
-    
-    private let timeInterval: TimeInterval
-    private let mainLoop:()->Void // Function-pointer to main loop
-    
-    init(timeInterval: TimeInterval, mainLoop:@escaping ()->Void ){
-        self.timeInterval = timeInterval
-        self.mainLoop = mainLoop
-    }
-    
-    private lazy var backgroundTimer: DispatchSourceTimer = {
-        let timer = DispatchSource.makeTimerSource()
-        timer.schedule(deadline: .now(), repeating:self.timeInterval)
-        timer.setEventHandler(handler: { [weak self] in
-            
-            AppNapController.shared.keepAlive()
-            self?.mainLoop()
-            
-        })
-        return timer
-    }()
-    
-    
-    // MARK: - Cycle Control
-    @Published public var status: Status = .stopped
-    
-    func run() {
-        if status != .running {
-            backgroundTimer.resume()
-            status = .running
-        }
-    }
-    
-    func stop() {
-        if status != .stopped {
-            backgroundTimer.suspend()
-            status = .stopped
-        }
-    }
-    
-   
-    
+	
+	private let timeInterval: TimeInterval
+	private let mainLoop:()->Void // Function-pointer to main loop
+	
+	init(timeInterval: TimeInterval, mainLoop:@escaping ()->Void, maxCycleTimeInMicroSeconds:TimeInterval = 300000){
+		self.timeInterval = timeInterval
+		self.mainLoop = mainLoop
+		self.maxCycleTime = maxCycleTimeInMicroSeconds
+	}
+	
+	private lazy var backgroundTimer: DispatchSourceTimer = {
+		let timer = DispatchSource.makeTimerSource()
+		timer.schedule(deadline: .now(), repeating:self.timeInterval)
+		timer.setEventHandler(handler: { [weak self] in
+			
+			// Prevent the App from napping (and the timer from pausing)
+			AppNapController.shared.keepAlive()
+			
+			let cycleStart = TimeStamp.CurrentTimeStamp
+			self!.mainLoop()
+			
+			// Calculate the cycletime
+			let currentCycleTime = (TimeStamp.CurrentTimeStamp-cycleStart)*1000
+			DispatchQueue.main.async {
+				self!.cycleTimeInMicroSeconds = currentCycleTime
+			}
+			
+			// Stop if PLC gets slow
+			let maxCycleTime = self!.maxCycleTime
+			if currentCycleTime > maxCycleTime{
+				self!.stop()
+			}
+			
+		})
+		return timer
+	}()
+	
+	
+	// MARK: - Cycle Control
+	@Published public var status: Status = .stopped
+	@Published var cycleTimeInMicroSeconds:TimeInterval = 0
+	@Published var maxCycleTime:TimeInterval
+	
+	func run() {
+		if status != .running {
+			backgroundTimer.resume()
+			DispatchQueue.main.async {
+				self.status = .running // At all times published variables should be changed on the main thread
+			}
+		}
+	}
+	
+	func stop() {
+		if status != .stopped {
+			backgroundTimer.suspend()
+			DispatchQueue.main.async {
+				self.status = .stopped // At all times published variables should be changed on the main thread
+			}
+		}
+	}
+	
 }
--- Sources/SoftPLC/PLCView.swift
+++ Sources/SoftPLC/PLCView.swift
@@ -14,7 +14,7 @@ public struct PLCView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @ObservedObject var plcBackGroundCyle:PLCBackgroundCycle // Always detect changes in the PLCs status
     @State private var runButtonState:Bool = false// Detect button actions that originated from here
-    @State private var simButtonState:Bool = true //TODO: - Sync with the actual state like runButtonState
+    @State private var simButtonState:Bool = true // TODO: - Sync with the actual state like runButtonState
     let togglePLCState:(_ newState:Bool)->Void
     let toggleSimulator:(_ newState:Bool)->Void
     
@@ -23,11 +23,15 @@ public struct PLCView: View {
         
         return VStack{
             Spacer()
-            RunStopView(buttonState: $runButtonState, plcIsRunning:(plcBackGroundCyle.status == .running))
-                .onAppear{runButtonState = (plcBackGroundCyle.status == .running)}
-                .onChange(of: runButtonState, perform: {togglePLCState($0)})
+			RunStopView(buttonState: $runButtonState,
+						plcIsRunning:(plcBackGroundCyle.status == .running),
+						cycleTime:Int(plcBackGroundCyle.cycleTimeInMicroSeconds)
+			)
+			.onAppear{runButtonState = (plcBackGroundCyle.status == .running)}
+			.onChange(of: runButtonState, perform: {togglePLCState($0)})
             Spacer()
             SimulatorView(buttonState: $simButtonState)
+				.onAppear{runButtonState = (plcBackGroundCyle.status == .running)}
                 .onChange(of: simButtonState, perform: {toggleSimulator($0)})
             Spacer()
         }
@@ -39,6 +43,7 @@ extension PLCView{
     public struct RunStopView: View {
         @Binding var buttonState:Bool
         let plcIsRunning:Bool
+		let cycleTime:Int
         
         public var body: some View {
             
@@ -51,11 +56,10 @@ extension PLCView{
                 .softToggleStyle(Circle(), padding: 20, pressedEffect: .hard)
                 .frame(width: 80)
 
-                Text(plcIsRunning ? "PLC in RUN!" : "PLC in STOP!")
+                Text(plcIsRunning ? "PLC in RUN!\n[\(cycleTime) µs]" : "PLC in STOP!")
                     .fontWeight(.bold)
                     .foregroundColor(.secondary)
-                    .frame(width: 100, alignment: .leading)
-
+                    .frame(width: 120, alignment: .leading)
                 Spacer()
             }
             
