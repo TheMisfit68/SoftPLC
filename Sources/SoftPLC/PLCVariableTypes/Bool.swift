33faf19141409b0098b0850f0da191916fbe1508
Jan Verrept <janverrept@me.com>
2021-07-16 18:07:25 +0200

    added conditional set and reset

--- Sources/SoftPLC/PLCVariableTypes/Bool.swift
+++ Sources/SoftPLC/PLCVariableTypes/Bool.swift
@@ -7,33 +7,48 @@
 
 import Foundation
 
-
+// MARK: - SET/RESET
 public extension Bool {
-        
-    mutating func set(){
-        self = true
-    }
-    
-    mutating func reset(){
-        self = false
-    }
-    
-    mutating func timed(using timer:DigitalTimer = DigitalTimer.OnDelay(time:1.0){} ) -> Bool{
-        timer.input = self
-        self = timer.output
-        return self
-    }
-    
-    func test(){
-        
-    }
-    
+	
+	mutating func set(){
+		self = true
+	}
+	
+	mutating func reset(){
+		self = false
+	}
+	
+	mutating func setConditional(_ condition:Bool){
+		if condition{
+			self.set()
+		}
+	}
+
+	mutating func resetConditional(_ condition:Bool){
+		if condition{
+			self.reset()
+		}
+	}
+	
 }
 
 public func set(_ boolean:inout Bool){
-    boolean.set()
+	boolean.set()
 }
 
 public func reset(_ boolean:inout Bool){
-    boolean.reset()
+	boolean.reset()
 }
+
+
+// MARK: - TIMING
+public extension Bool {
+	
+	mutating func timed(using timer:DigitalTimer = DigitalTimer.OnDelay(time:1.0){} ) -> Bool{
+		timer.input = self
+		self = timer.output
+		return self
+	}
+	
+}
+
