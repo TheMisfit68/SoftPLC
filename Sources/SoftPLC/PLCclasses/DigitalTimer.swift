e22da414d5efe5884873813055683c81974228da
TheMisfit68 <janverrept@me.com>
2024-02-17 21:31:39 +0100

    Implement canceling of Digital Timers using a workItem

--- Sources/SoftPLC/PLCclasses/DigitalTimer.swift
+++ Sources/SoftPLC/PLCclasses/DigitalTimer.swift
@@ -9,113 +9,147 @@
 import Foundation
 
 open class DigitalTimer:PLCClass{
-    
-    private var risingEdge:Bool = false
-    private var falingEdge:Bool = false
-    
-    public var input:Bool = false
-    public let timeValue:TimeInterval
-    public var output:Bool = false
-    
-    public init(time:TimeInterval){
-        self.timeValue = time
-    }
+	
+	private var workItem: DispatchWorkItem?
+
+	private var actionToken: UUID?
+	private var risingEdge:Bool = false
+	private var fallingEdge:Bool = false
+	
+	public var input:Bool = false
+	public let timeValue:TimeInterval
+	public var output:Bool = false
+	
+	public init(time:TimeInterval){
+		self.timeValue = time
+	}
+	
+	func delayAction(_ delayedAction: @escaping () -> Void) {
+		
+		// Cancel the previous work item if it exists
+		workItem?.cancel()
+		
+		// Create a new work item
+		let newWorkItem = DispatchWorkItem(block: delayedAction)
+		self.workItem = newWorkItem
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + timeValue, execute: newWorkItem)
+		 
+	}
+	
+	public func reset() {
+		
+		workItem?.cancel()
+		self.output = false
+		
+	}
+	
 }
 
 extension DigitalTimer{
-    
-    open class OnDelay:DigitalTimer{
-        
-        public var action:() -> Void
-        
-        public init(time:TimeInterval = 1.0, _ action:@escaping ()->Void = {} ){
-            self.action = action
-            super.init(time: time)
-        }
-        
-        
-        public override var input:Bool {
-            didSet{
-                risingEdge = input && !oldValue
-                falingEdge = !input && oldValue
-                
-                if risingEdge{
-                    DispatchQueue.main.asyncAfter(deadline: .now() + timeValue) {
-                        self.output = self.input
-                        if self.output{
-                            self.action()
-                        }
-                    }
-                }else if falingEdge{
-                    output = input
-                }
-            }
-        }
-        
-    }
+	
+	open class OnDelay:DigitalTimer{
+		
+		public var action:() -> Void
+		
+		public init(time:TimeInterval = 1.0, _ action:@escaping ()->Void = {} ){
+			self.action = action
+			super.init(time: time)
+		}
+		
+		
+		public override var input:Bool {
+			
+			didSet{
+				risingEdge = input && !oldValue
+				fallingEdge = !input && oldValue
+				
+				if risingEdge{
+					
+					delayAction{
+						self.output = self.input
+						if self.output{
+							self.action()
+						}
+					}
+					
+				}else if fallingEdge{
+					output = input
+				}
+			}
+			
+		}
+	}
 }
 
 
 extension DigitalTimer{
-    
-    open class OffDelay:DigitalTimer{
-        
-        public override var input:Bool {
-            didSet{
-                risingEdge = input && !oldValue
-                falingEdge = !input && oldValue
-                
-                if risingEdge{
-                    output = input
-                }else if falingEdge{
-                    DispatchQueue.main.asyncAfter(deadline: .now() + timeValue) {self.output = self.input}
-                }
-            }
-        }
-        
-    }
+	
+	open class OffDelay:DigitalTimer{
+		
+		public override var input:Bool {
+			
+			didSet{
+				risingEdge = input && !oldValue
+				fallingEdge = !input && oldValue
+				
+				if risingEdge{
+					output = input
+				}else if fallingEdge{
+					
+					delayAction{self.output = self.input}
+					
+				}
+				
+			}
+			
+		}
+	}
 }
 
 extension DigitalTimer{
-    
-    open class PulsLimition:DigitalTimer{
-        
-        public override var input:Bool {
-            didSet{
-                risingEdge = input && !oldValue
-                falingEdge = !input && oldValue 
-                
-                if risingEdge{
-                    output = input
-                    DispatchQueue.main.asyncAfter(deadline: .now() + timeValue) {
-                        self.output = false
-                    }
-                }else if falingEdge{
-                    output = input
-                }
-            }
-        }
-        
-    }
+	
+	open class PulsLimition:DigitalTimer{
+		
+		public override var input:Bool {
+			didSet{
+				
+				risingEdge = input && !oldValue
+				fallingEdge = !input && oldValue
+				
+				if risingEdge{
+					
+					output = input
+					delayAction{self.output = false}
+					
+				}else if fallingEdge{
+					output = input
+				}
+				
+			}
+			
+		}
+	}
 }
 
 extension DigitalTimer{
-    
-    open class ExactPuls:DigitalTimer{
-        
-        public override var input:Bool {
-            didSet{
-                risingEdge = input && !oldValue
-                falingEdge = !input && oldValue
-                
-                if risingEdge{
-                    output = input
-                    DispatchQueue.main.asyncAfter(deadline: .now() + timeValue) {self.output = false}
-                }else if falingEdge{
-                    // Do nothing on the faling edge
-                }
-            }
-        }
-        
-    }
+	
+	open class ExactPuls:DigitalTimer{
+		
+		public override var input:Bool {
+			didSet{
+				risingEdge = input && !oldValue
+				fallingEdge = !input && oldValue
+				
+				if risingEdge{
+					output = input
+					delayAction{self.output = false}
+				}else if fallingEdge{
+					// Do nothing on the faling edge
+				}
+				
+			}
+		}
+		
+	}
 }
